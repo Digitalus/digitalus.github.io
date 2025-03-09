f5998e492ad0806dd3ec75167b0722273cf6dac6
Digitalus <Digitalus@users.noreply.github.com>
2025-03-09 13:59:12 -0500

    Added some AJAX and prepping to load goodie examples.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
 // Add your JavaScript code here if you need any interactivity
+
+$.ajax({
+    type: "method",
+    url: "url",
+    data: "data",
+    dataType: "dataType",
+    success: function (response) {
+        
+    }
+});
